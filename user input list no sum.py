13a1eba636564a145566a0ee192b192624510036
Mariana Barcelos <marianabarcelos13s@hotmail.com>
2019-10-14 12:24:41 -0300

    '++'

--- user input list no sum.py
+++ user input list no sum.py
@@ -1,7 +1,10 @@
-print('Program to Sum all the numbers given by user')
+print('**Program to Sum all the numbers given by user**')
 number=[]
+print('Hello user!')
 n=int(input('Enter no of times you want to enter number? '))
 for i in range(n):
+    name=input('Hi! whats you name?')
+    print(f'Hey {name} lets start!')
     n1=int(input('Enter number here = '))
     number.append(n1)
 print('your input number list is' ,number)
