8e490dec9e891c7b7ddf24a5724ceb407372d093
Sayed Reyaz <sreyaz90@outlook.com>
2019-10-15 11:00:20 +0530

    name should be out of loop
    otherwise it will ask again ang when loop will triggger

--- user input list no sum.py
+++ user input list no sum.py
@@ -2,9 +2,9 @@ print('**Program to Sum all the numbers given by user**')
 number=[]
 print('Hello user!')
 n=int(input('Enter no of times you want to enter number? '))
+name=input('Hi! whats you name?')
+print(f'Hey {name} lets start!')
 for i in range(n):
-    name=input('Hi! whats you name?')
-    print(f'Hey {name} lets start!')
     n1=int(input('Enter number here = '))
     number.append(n1)
 print('your input number list is' ,number)
